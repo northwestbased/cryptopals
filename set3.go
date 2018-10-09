f03230edd29af632b573c64e09a2726fb63b1ed1
northwestbased <northwestbased@gmail.com>
2018-10-09 15:59:54 -0700

    Problems 21-24 completed

--- set3.go
+++ set3.go
@@ -4,6 +4,9 @@ import (
 	"encoding/base64"
 	"encoding/binary"
 	"log"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func GetRandomString() []byte {
@@ -195,4 +198,168 @@ func BreakCTRStatistically() {
 	for _, b := range blocks {
 		log.Println(string(b))
 	}
+}
+
+// based on pseudocode from https://en.wikipedia.org/w/index.php?title=Mersenne_Twister&oldid=408201780
+type MersenneTwister struct {
+	state []uint32
+	index int
+}
+
+// Initialize the generator from a seed
+func (mt *MersenneTwister) init(seed uint32) {
+	mt.state = make([]uint32, 624)
+	mt.state[0] = seed
+	for i := 1; i < 624; i++ { // loop over each other element
+		mt.state[i] = (1812433253 * (mt.state[i-1] ^ (mt.state[i-1] >> 30))) + uint32(i)
+	}
+}
+
+// Extract a tempered pseudorandom number based on the mt.index-th value,
+// calling generateNumbers() every 624 numbers
+func (mt *MersenneTwister) ExtractNumber() uint32 {
+	if mt.index == 0 {
+		mt.generateNumbers()
+	}
+
+	y := mt.state[mt.index]
+	y ^= (y >> 11)
+	y ^= (y << 7) & 0x9d2c5680
+	y ^= (y << 15) & 0xefc60000
+	y ^= (y >> 18)
+
+	mt.index = (mt.index + 1) % 624
+	return y
+}
+
+// Generate an array of 624 untempered numbers
+func (mt *MersenneTwister) generateNumbers() {
+	for i := 0; i < 624; i++ {
+		y := mt.state[i]&0x80000000 + mt.state[(i+1)%624]&0x7FFFFFFF
+		mt.state[i] = mt.state[(i+397)%624] ^ (y >> 1)
+		if (y % 2) == 1 { // y is odd
+			mt.state[i] ^= 2567483615
+		}
+	}
+}
+
+func WaitThenGenerateNum() uint32 {
+	mt := MersenneTwister{}
+	wait1 := GetRandomInt(960) + 40
+	wait2 := GetRandomInt(960) + 40
+	time.Sleep(time.Duration(wait1) * time.Second)
+	mt.init(uint32(time.Now().Unix()))
+	log.Println("seed done")
+	time.Sleep(time.Duration(wait2) * time.Second)
+	return mt.ExtractNumber()
+}
+
+func CrackSeed(firstNum uint32) {
+	t := time.Now().Unix()
+	for i := 0; i < 3000; i++ {
+		mt := MersenneTwister{}
+		seed := uint32(t - int64(i))
+		log.Println(seed)
+
+		mt.init(seed)
+		if mt.ExtractNumber() == firstNum {
+			log.Printf("found that seed!!!!! seed %v %v", seed, i)
+			return
+		}
+	}
+	log.Println("did not find seed")
+}
+
+func CloneMTwisterState(state []uint32) []uint32 {
+	reverseRightShift := func(product uint32, offset uint) uint32 {
+		mask := uint32(0xFFFFFFFF << (32 - offset))
+		y := product & mask
+
+		for i := 32 - offset; i >= 0 && i < 33; i-- {
+			mask = 1 << i
+			pbit := product & mask
+			ybit := (y >> offset) & mask
+			y |= pbit ^ ybit
+		}
+		return y
+
+	}
+
+	reverseLeftShift := func(product uint32, offset uint, and uint32) uint32 {
+		mask := uint32(0xFFFFFFFF >> (32 - offset))
+		y := product & mask
+		for i := offset; i < 32; i++ {
+			mask = 1 << i
+			pbit := product & mask
+			ybit := (y << offset) & mask
+			andBit := and & mask
+			y |= pbit ^ ybit&andBit
+		}
+		return y
+	}
+
+	for i, s := range state {
+		s = reverseRightShift(s, 18)
+		s = reverseLeftShift(s, 15, 0xefc60000)
+		s = reverseLeftShift(s, 7, 0x9d2c5680)
+		state[i] = reverseRightShift(s, 11)
+	}
+	return state
+}
+
+func MT19937StreamCipher(text []byte, key uint16) []byte {
+	var output []byte
+
+	mt := MersenneTwister{}
+	mt.init(uint32(key))
+	var keyStreamBuffer []byte
+	for _, t := range text {
+		if len(keyStreamBuffer) == 0 {
+			num := mt.ExtractNumber()
+			str := strconv.Itoa(int(num))
+			keyStreamBuffer = append(keyStreamBuffer, []byte(str)...)
+		}
+		output = append(output, t^keyStreamBuffer[0])
+		keyStreamBuffer = keyStreamBuffer[1:]
+	}
+	return output
+}
+
+func MT19937StreamCipherEncryptWithPrefix(text []byte) []byte {
+	key := uint16(GetRandomInt(0x10000))
+	prefixLength := GetRandomInt(1000)
+	prefix := Key(prefixLength)
+	text = append(prefix, text...)
+	return MT19937StreamCipher(text, key)
+}
+
+func BreakMTStreamCipherWithPrefix() {
+	ct := MT19937StreamCipherEncryptWithPrefix([]byte("AAAAAAAAAAAAAAAAAAA"))
+	for i := 0; i < 0x10000; i++ {
+		key := uint16(i)
+		out := MT19937StreamCipher(ct, key)
+		if strings.Contains(string(out), "AAAAAAAAAAAAAAAAAAA") {
+			log.Println("found it!", key, out)
+		}
+	}
+
+}
+
+func CreatePasswordResetToken() []byte {
+	seed := uint32(time.Now().Unix())
+	prefixLength := GetRandomInt(1000)
+	prefix := Key(prefixLength)
+	text := append(prefix, []byte("password reset")...)
+	return MT19937StreamCipher(text, uint16(seed))
+}
+
+func CheckPasswordResetToken(ct []byte) bool {
+	time := uint16(time.Now().Unix())
+	for i := 0; i < 10; i++ {
+		pt := MT19937StreamCipher(ct, uint16(time-uint16(i)))
+		if strings.Contains(string(pt), "password reset") {
+			return true
+		}
+	}
+	return false
 }
--- set3_test.go
+++ set3_test.go
@@ -29,3 +29,45 @@ func Test_20(t *testing.T) {
 	log.Printf("20")
 	BreakCTRStatistically()
 }
+
+func Test_21(t *testing.T) {
+	log.Printf("Mersenne Twister's first 3 outputs:")
+	mt := MersenneTwister{}
+	mt.init(100)
+	log.Println(mt.ExtractNumber())
+	log.Println(mt.ExtractNumber())
+	log.Println(mt.ExtractNumber())
+}
+
+func Test_22(t *testing.T) {
+	//num := WaitThenGenerateNum()
+	//log.Println("num is", num)
+	//CrackSeed(num)
+	//commenting the above three lines out for now... they take too long!
+}
+
+func Test_23(t *testing.T) {
+	mt := MersenneTwister{}
+	mt.init(uint32(GetRandomInt(0x100000000)))
+	var numbers []uint32
+	for i := 0; i < 624; i++ {
+		numbers = append(numbers, mt.ExtractNumber())
+	}
+	newState := CloneMTwisterState(numbers)
+	mtCopy := MersenneTwister{}
+	mtCopy.state = newState
+	if mtCopy.ExtractNumber() != mt.ExtractNumber() {
+		t.Fatal("didn't work")
+	}
+	log.Println(mtCopy.ExtractNumber(), mt.ExtractNumber())
+	log.Println(mtCopy.ExtractNumber(), mt.ExtractNumber())
+	log.Println(mtCopy.ExtractNumber(), mt.ExtractNumber())
+
+}
+
+func Test_24(t *testing.T) {
+	BreakMTStreamCipherWithPrefix()
+
+	ct := CreatePasswordResetToken()
+	log.Println("valid token?", CheckPasswordResetToken(ct))
+}
