12820f689a56b694bc8d6cf309a329f6cf7cc241
northwestbased <northwestbased@gmail.com>
2018-10-09 21:23:35 -0700

    Problems 25-28 completed

---
+++ set4_test.go
@@ -0,0 +1,40 @@
+package cryptopals
+
+import (
+	"encoding/base64"
+	"log"
+	"testing"
+)
+
+func Test_25(t *testing.T) {
+	lines, err := ReadFileByLine("input/25.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var ct []byte
+	for _, l := range lines {
+		dl, _ := base64.StdEncoding.DecodeString(l)
+		ct = append(ct, dl...)
+	}
+
+	pt := AESInECBModeDecrypt(ct, []byte("YELLOW SUBMARINE"))
+
+	ct, updateFunc := CTRRandomAccessReadWrite(pt)
+	pt = BreakCTRRandomAccess(ct, updateFunc)
+	log.Println(string(pt))
+}
+
+func Test_26(t *testing.T) {
+	log.Println(createBitflippedAdminCTR())
+}
+
+func Test_27(t *testing.T) {
+	log.Println("out", findCBCKey())
+}
+
+func Test_28(t *testing.T) {
+	log.Println(SHA1([]byte("text"), []byte("key")))
+	log.Println(SHA1([]byte("texu"), []byte("key")))
+
+}
